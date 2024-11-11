9bc10d3509b26a9711580e89f00cf537c74eff0c
Felix Lyons <felix.lyons204@gmail.com>
2024-11-11 14:00:47 -0500

    Fixed API Call so it calls on London, updated pom.xml to allow okhttp3 to read API

--- src/main/java/org/weatherapp/WeatherService.java
+++ src/main/java/org/weatherapp/WeatherService.java
@@ -1,13 +1,20 @@
 package org.weatherapp;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-
 import netscape.javascript.JSObject;
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 // Service class to fetch weather data (currently simulated)
 public class WeatherService {
@@ -16,42 +23,31 @@ public class WeatherService {
     private double temperature;
     private String condition;
     private int humidity;
+    private JSONObject information;
+
+    private static String test;
 
     public WeatherService(){
         String apiKey = "a7053dadfa852680faa79393bbab3b4f";
         String city = "London";
         String urlString = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
-        StringBuilder response = new StringBuilder();
+        //StringBuilder response = new StringBuilder();
 
+        final OkHttpClient client = new OkHttpClient().newBuilder().build();
+        final Request request = new Request.Builder().url(urlString).build();
         try {
-            // Create URL object
-            URL url = new URL(urlString);
-            // Open a connection to the URL
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("GET");
-
-            // Check if the request was successful
-            int responseCode = connection.getResponseCode();
-            if (responseCode == HttpURLConnection.HTTP_OK) { // 200
-                BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-                String inputLine;
-
-                // Read the response
-                while ((inputLine = in.readLine()) != null) {
-                    response.append(inputLine);
-                }
-                in.close();
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
+            final Response response = client.newCall(request).execute();
+            final JSONObject responseBody = new JSONObject(response.body().string());
+            information = responseBody;
+        }
+        catch (IOException | JSONException event) {
+            throw new RuntimeException(event);
         }
 
-        JSONObject jsonObject = new JSONObject(response);
-
-        location = jsonObject.getString("main");
-        temperature = jsonObject.getJSONObject("main").getDouble("temp");
-        condition = jsonObject.getJSONArray("weather").getJSONObject(2).getString("description");
-        humidity = jsonObject.getJSONObject("main").getInt("humidity");
+        location = information.getString("name");
+        temperature = information.getJSONObject("main").getDouble("temp");
+        condition = information.getJSONArray("weather").getJSONObject(0).getString("description");
+        humidity = information.getJSONObject("main").getInt("humidity");
     }
 
     // Method to get current weather for a location
