4616c798a81bb196c18ebf87b153a112b45be094
Felix Lyons <felix.lyons204@gmail.com>
2024-11-11 14:42:02 -0500

    Changed around WeatherService.java so now API Call is in getCurrentWeather method and API can now be called on any location (provided location is an accurate city, otherwise it will fail)

--- src/main/java/org/weatherapp/WeatherService.java
+++ src/main/java/org/weatherapp/WeatherService.java
@@ -1,6 +1,9 @@
 package org.weatherapp;
 
 import java.io.IOException;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -10,42 +13,42 @@ import okhttp3.Response;
 
 // Service class to fetch weather data (currently simulated)
 public class WeatherService {
-
-    private String location;
-    private double temperature;
-    private String condition;
-    private int humidity;
     private JSONObject information;
 
     public WeatherService(){
+        information = new JSONObject();
+    }
+
+    // Method to get current weather for a location
+    public WeatherData getCurrentWeather(String loc) {
+        //Rounding for temperature
+        DecimalFormat df = new DecimalFormat("#.##");
+        df.setRoundingMode(RoundingMode.HALF_UP);
+
+        //Call API with actual city
         String apiKey = "a7053dadfa852680faa79393bbab3b4f";
-        String city = "London";
+        String city = loc;
         String urlString = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
 
         final OkHttpClient client = new OkHttpClient().newBuilder().build();
         final Request request = new Request.Builder().url(urlString).build();
         try {
             final Response response = client.newCall(request).execute();
-            final JSONObject responseBody = new JSONObject(response.body().string());
-            information = responseBody;
+            information = new JSONObject(response.body().string());
         }
         catch (IOException | JSONException event) {
             throw new RuntimeException(event);
         }
 
-        location = information.getString("name");
-        temperature = information.getJSONObject("main").getDouble("temp");
-        condition = information.getJSONArray("weather").getJSONObject(0).getString("description");
-        humidity = information.getJSONObject("main").getInt("humidity");
-    }
+        String location = information.getString("name");
+        double temperature = information.getJSONObject("main").getDouble("temp");
+        String condition = information.getJSONArray("weather").getJSONObject(0).getString("description");
+        int humidity = information.getJSONObject("main").getInt("humidity");
 
-    // Method to get current weather for a location
-    public WeatherData getCurrentWeather(String location) {
         // Simulate fetching weather data
-        // TODO: Implement API calls here
         WeatherData weatherData = new WeatherData();
-        weatherData.setLocation(this.location);
-        weatherData.setTemperature(temperature);
+        weatherData.setLocation(location);
+        weatherData.setTemperature(Double.parseDouble(df.format(temperature-273.15)));
         weatherData.setCondition(condition);
         weatherData.setHumidity(humidity);
 
