a350880a6a4cb197109c93eec24e4bbcac4ead68
mengzh-z <mengzhou.zhang@mail.utoronto.ca>
2024-11-29 18:35:09 -0500

    Continue working on the history usecase.

--- src/main/java/interface_adapter/get_history/GetHistoryViewModel.java
+++ src/main/java/interface_adapter/get_history/GetHistoryViewModel.java
@@ -1,4 +1,23 @@
 package interface_adapter.get_history;
 
-public class GetHistoryViewModel {
+import interface_adapter.ViewModel;
+import interface_adapter.check_city.CheckCityState;
+
+/**
+ * The ViewModel for the GetHistory View.
+ */
+public class GetHistoryViewModel extends ViewModel<GetHistoryState> {
+
+    public static final String GET_HISTORY_TITLE_LABEL = "Weather History for the Past 9 Hours (3-Hour Intervals)";
+    public static final String CITY_NAME_LABEL = "City Name:";
+    public static final String THREE_HOURS_AGO_LABEL = "3 Hours Ago";
+    public static final String SIX_HOURS_AGO_LABEL = "6 Hours Ago";
+    public static final String NINE_HOURS_AGO_LABEL = "9 Hours Ago";
+
+    public static final String BACK_BUTTON_LABEL = "Back";
+
+    public GetHistoryViewModel() {
+        super("get history");
+        setState(new GetHistoryState());
+    }
 }
