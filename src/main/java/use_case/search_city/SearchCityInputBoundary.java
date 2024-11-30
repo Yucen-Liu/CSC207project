6f9f40e2a578a1e2633c68e4bcea82ca7e234489
Yucen_Liu <yucen.liu@mail.utoronto.ca>
2024-11-30 02:14:32 -0500

    updated search city use cases, but there is static error need to be resolved

--- src/main/java/use_case/search_city/SearchCityInputBoundary.java
+++ src/main/java/use_case/search_city/SearchCityInputBoundary.java
@@ -1,4 +1,25 @@
 package use_case.search_city;
 
+import use_case.check_city.CheckCityInputData;
+
+/**
+ * Input Boundary for actions which are related to the home page.
+ */
 public interface SearchCityInputBoundary {
+
+    /**
+     * Executes the home use case.
+     * @param searchcityInputData the input data
+     */
+    void execute(SearchCityInputData searchcityInputData);
+
+    /**
+     * Executes the switch to login view use case.
+     */
+    void switchToLoginView();
+
+    /**
+     * Executes the switch to signup view use case.
+     */
+    void switchToSignupView();
 }
