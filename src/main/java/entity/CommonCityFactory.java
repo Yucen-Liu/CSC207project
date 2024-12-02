d128cfae926009e72a9f4ccc35c604ff8f7bc73d
Eric Ju <eric.ju@mail.utoronto.ca>
2024-12-02 08:26:55 -0500

    change

--- src/main/java/entity/CommonCityFactory.java
+++ src/main/java/entity/CommonCityFactory.java
@@ -2,6 +2,7 @@ package entity;
 /**
  * Factory for creating CommonCity objects.
  */
+// City Factory
 public class CommonCityFactory implements CityFactory {
     @Override
     public City create(String location, double temperature, String condition, int humidity) {
