749897c9f963ca24a60de0af0d8978f08a90c4a1
Eric Ju <eric.ju@mail.utoronto.ca>
2024-12-02 03:33:57 -0500

    update usecase

--- src/main/java/use_case/manage_sort/SortCitiesInteractor.java
+++ src/main/java/use_case/manage_sort/SortCitiesInteractor.java
@@ -1,9 +1,10 @@
 package use_case.manage_sort;
 
-import java.util.ArrayList;
+import java.util.List;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.List;
+import java.util.ArrayList;
+
 
 import entity.CommonCity;
 
@@ -12,51 +13,63 @@ import entity.CommonCity;
  */
 public class SortCitiesInteractor implements SortCitiesInputBoundary {
 
-    // Initialize Part
-    private final SortCitiesDataAccessInterface dataAccess;
+    // Initialize Part 1 We need to create an outputBoundary
     private final SortCitiesOutputBoundary outputBoundary;
+    // Initialize Part 2: dataAccessInterface in order to get weather through City name
+    private final SortCitiesDataAccessInterface dataAccessInterface;
+
+
 
     // 生成一个 'Interactor'需要input：1. list of CommonCity (saved list) 2. outputBoundary
-    public SortCitiesInteractor(SortCitiesDataAccessInterface dataAccess, SortCitiesOutputBoundary outputBoundary) {
-        this.dataAccess = dataAccess;
+    public SortCitiesInteractor(SortCitiesDataAccessInterface dataAccessInterface, SortCitiesOutputBoundary outputBoundary){
+        this.dataAccessInterface = dataAccessInterface;
         this.outputBoundary = outputBoundary;
     }
 
+
+
     // Override the sort method from input boundary
     // The only input is InputData class, and we don't care what data is that, we just assume there's correct data input
+    @Override
+    public void switchToSearchCityView(){
+    outputBoundary.switchToSearchCityView();}
+
+
 
     @Override
-    public void execute(SortCitiesInputData inputData) {
-        String criterion = inputData.getCriterion();
-        List<CommonCity> savedCities = new ArrayList<>();
-        for (String name: inputData.getSavedCityNames()){
-            CommonCity city = (CommonCity) dataAccess.getCurWeather(name);
-            savedCities.add(city);}
+    public void execute(SortCitiesInputData inputData){
 
-        switch (criterion.toLowerCase()) {
+        String theCriterion = inputData.getCriterion();
+
+        List<CommonCity> newSavedCities = new ArrayList<>();
+        // Use the while loop to reach all Common City in teh list
+        List<String> cityNames = inputData.getSavedCityNames();
+
+        int index = 0;
+        while (index < cityNames.size()){
+            String name = cityNames.get(index);
+            CommonCity city = (CommonCity) dataAccessInterface.getCurWeather(name);
+            newSavedCities.add(city);
+            index++;
+        }
+        switch (theCriterion.toLowerCase()) {
             case "temperature":
-                Collections.sort(savedCities, Comparator.comparingDouble(CommonCity::getTemperature));
+                Collections.sort(newSavedCities, Comparator.comparingDouble(CommonCity::getTemperature));
                 break;
             case "condition":
-                Collections.sort(savedCities, Comparator.comparing(CommonCity::getCondition));
+                Collections.sort(newSavedCities, Comparator.comparing(CommonCity::getCondition));
                 break;
             case "humidity":
-                Collections.sort(savedCities, Comparator.comparingInt(CommonCity::getHumidity));
+                Collections.sort(newSavedCities, Comparator.comparingInt(CommonCity::getHumidity));
                 break;
             default:
-                throw new IllegalArgumentException("Invalid sort criterion: " + criterion);
+                throw new IllegalArgumentException("Invalid sort criterion: " + theCriterion);
         }
-
         // Create output data and pass it to the output boundary
         // Here 我们的 savedCities 是 sort好了，生成一个新的Output Data class 并扔给他 sorted list,
-        SortCitiesOutputData outputData = new SortCitiesOutputData(savedCities);
-
+        SortCitiesOutputData outputData = new SortCitiesOutputData(newSavedCities);
         // 再把这个data存到 'OutputBoundary' which is used by 'Presenter'
         outputBoundary.presentSortedCities(outputData);
     }
 
-    @Override
-    public void switchToSearchCityView() {outputBoundary.switchToSearchCityView();}
-
-    }
-
+}
