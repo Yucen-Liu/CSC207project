bbd5d0cb832ebc3147114e622ae294f4c5c39a18
mengzh-z <mengzhou.zhang@mail.utoronto.ca>
2024-12-01 23:46:03 -0500

    fix

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -20,7 +20,7 @@ public class Main {
                 .addGetForecastUseCase()
 //                .addManageSortUseCase()
                 .addNearbyCitiesUseCase()
-                .addSearchCityUseCase()
+//                .addSearchCityUseCase()
                 .build();
 
 
