7efdd00e29349e5b4add389ff5bd82274451a112
Eric Ju <eric.ju@mail.utoronto.ca>
2024-12-03 19:03:57 -0500

    add improvements

--- src/main/java/interface_adapter/ViewManagerModel.java
+++ src/main/java/interface_adapter/ViewManagerModel.java
@@ -4,6 +4,7 @@ package interface_adapter;
  * Model for the View Manager. Its state is the name of the View which
  * is currently active. An initial state of "" is used.
  */
+// extend the ViewModel
 public class ViewManagerModel extends ViewModel<String> {
 
     public ViewManagerModel() {
